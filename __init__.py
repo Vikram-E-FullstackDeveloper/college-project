d6a6a569190c770aadc6d8e8e8548e52af7d0c8b
Vikram.E <vikramilango2005@gmail.com>
2025-12-27 16:35:46 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
-"""Subpackage containing all of pip's command line interface related code"""
+from __future__ import annotations
 
-# This file intentionally does not import submodules
+__version__ = "25.2"
+
+
+def main(args: list[str] | None = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
+
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
+
+    return _wrapper(args)
